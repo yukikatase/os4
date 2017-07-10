bef9ccf4cd537a82e7d854f65ffe822924ca7693
yukikatase <arasiusu@gmail.com>
2017-07-10 11:47:44 +0900

    7/10 ピンポンちょっとできた
    跳ね返るだけ

--- pingpong.c
+++ pingpong.c
@@ -4,7 +4,16 @@
 #define SCREEN_WIDTH  320
 
 char* pptr = (char *)(0xa0000 + SCREEN_WIDTH - 10);
+char* pptr2 = (char *)(0xa0000 + SCREEN_WIDTH * 2 - 10);
+char* pptr3 = (char *)(0xa0000 + SCREEN_WIDTH * 3 - 10);
+char* pptr4 = (char *)(0xa0000 + SCREEN_WIDTH * 4 - 10);
+char* pptr5 = (char *)(0xa0000 + SCREEN_WIDTH * 5 - 10);
+char* pptr6 = (char *)(0xa0000 + SCREEN_WIDTH * 6 - 10);
+char* pptr7 = (char *)(0xa0000 + SCREEN_WIDTH * 7 - 10);
+
 char* bptr = (char*)(0xa0000 + SCREEN_WIDTH * 80);
+
+int* i = 0;
 
 int kbd_handler();
 int timer_handler();
@@ -24,6 +33,12 @@ int main() {
 
   *bptr = 14;
   *pptr = 15;
+  *pptr2 = 15;
+  *pptr3 = 15;
+  *pptr4 = 15;
+  *pptr5 = 15;
+  *pptr6 = 15;
+  *pptr7 = 15;
 
   /* 現状では何もおこらないが、取りあえずソフトウェア割り込みをかけてみる */
   syscall(1, 0);
@@ -38,14 +53,40 @@ int kbd_handler() {
 
   /* ラケットを消去 */
   *pptr = 0;
+  *pptr2 = 0;
+  *pptr3 = 0;
+  *pptr4 = 0;
+  *pptr5 = 0;
+  *pptr6 = 0;
+  *pptr7 = 0;
 
   /* ラケットの位置を変更 */
   pptr += SCREEN_WIDTH;
-  if (pptr > ((char*)0xa0000) + SCREEN_WIDTH * 80)
+  pptr2 += SCREEN_WIDTH;
+  pptr3 += SCREEN_WIDTH;
+  pptr4 += SCREEN_WIDTH;
+  pptr5 += SCREEN_WIDTH;
+  pptr6 += SCREEN_WIDTH;
+  pptr7 += SCREEN_WIDTH;
+
+  if (pptr > ((char*)0xa0000) + SCREEN_WIDTH * 90){
     pptr = ((char*)0xa0000) + SCREEN_WIDTH - 10;
+    pptr2 = ((char*)0xa0000) + SCREEN_WIDTH * 2 - 10;
+    pptr3 = ((char*)0xa0000) + SCREEN_WIDTH * 3 - 10;
+    pptr4 = ((char*)0xa0000) + SCREEN_WIDTH * 4 - 10;
+    pptr5 = ((char*)0xa0000) + SCREEN_WIDTH * 5 - 10;
+    pptr6 = ((char*)0xa0000) + SCREEN_WIDTH * 6 - 10;
+    pptr7 = ((char*)0xa0000) + SCREEN_WIDTH * 7 - 10;
+  }
 
   /* ラケットを描画 */
   *pptr = 15;
+  *pptr2 = 15;
+  *pptr3 = 15;
+  *pptr4 = 15;
+  *pptr5 = 15;
+  *pptr6 = 15;
+  *pptr7 = 15;
 }
 
 int timer_handler() {
@@ -55,7 +96,16 @@ int timer_handler() {
   *bptr = 0;
 
   /* ボールの位置を変更 */
-  bptr++;
+  if(i == 0){
+    bptr++;
+  }
+  if(i == 1){
+    bptr--;
+  }
+
+  if(pptr == bptr){
+    i = 1;
+  }
 
   /* ボールを描画 */
   *bptr = 15;
